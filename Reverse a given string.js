0f4f1bc1e2f248179ff9e2119b175f32289a1677
Muhamad Adi Prasetyo <88260025+xTaMaox@users.noreply.github.com>
2022-08-06 07:27:54 +0700

    Add files via upload

---
+++ Reverse a given string.js
@@ -0,0 +1,20 @@
+function FirstReverse(str) {
+  /*
+    There are two ways to do this.
+    In-place by swapping individual array elements,
+    or simply splitting and reversing.
+    In the interest of time constraints,
+    I opted for the simpler method.
+  */
+
+  var charList = str.split("");
+  var stringRes = "";
+
+  charList.reverse();
+  stringRes = charList.join("");
+  return stringRes;
+}
+
+// Example output
+var outputValue = FirstReverse("quick-brown-fox");
+console.log(outputValue);
